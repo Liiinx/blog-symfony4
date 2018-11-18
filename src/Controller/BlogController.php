ec2844502123c7b059c330ef1e3506ae10ec3239
Felix Tuffreaud <felix.tuffreaud@laposte.net>
2018-11-18 17:40:29 +0100

    method showAllByCategory

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,4 +22,21 @@ class BlogController extends AbstractController
         //var_dump($slug);
         return $this->render('blog/blog.html.twig', ['slug' => $slug]);
     }
+
+
+    /**
+     * @Route("/blog/category/{category}/all", name="all_categories")
+     */
+    public function showAllByCategory($category)
+    {
+        $categories = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findBy(['name' => $category]);
+            //var_dump($categories);
+
+        $categoryArticles = $categories->getArticles();
+        var_dump($categoryArticles);
+
+    }
+
 }
